60bd44dcb40fa7408260c2a22dbdb61565610208
Denys <yevtushenko.denis@gmail.com>
2019-01-26 23:16:06 +0200

    Updates

---
+++ precache-manifest.8f09569cd5f1e99269641120e870de78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ef3642d99443e181ff7",
+    "url": "/todolist/static/css/main.0603e9a1.chunk.css"
+  },
+  {
+    "revision": "5ef3642d99443e181ff7",
+    "url": "/todolist/static/js/main.5ef3642d.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/todolist/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "14752cb12ce247ec1c9eb077e47745bd",
+    "url": "/todolist/index.html"
+  }
+];
